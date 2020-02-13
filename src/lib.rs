c63573a779357d5d27c2b486cf9593a7fcda5657
Linus Färnstrand <faern@faern.net>
2020-02-13 07:54:48 +0100

    Initial commit with simple working trigger
    Including a few tests for both tokio and async-std

---
+++ src/lib.rs
@@ -0,0 +1,82 @@
+use std::mem;
+use std::pin::Pin;
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc, Mutex,
+};
+use std::task::{Context, Poll, Waker};
+
+pub fn trigger() -> (Trigger, Listener) {
+    let inner = Arc::new(Inner {
+        complete: AtomicBool::new(false),
+        tasks: Mutex::new(Vec::new()),
+    });
+    let trigger = Trigger {
+        inner: inner.clone(),
+    };
+    let listener = Listener { inner };
+    (trigger, listener)
+}
+
+#[derive(Clone)]
+pub struct Trigger {
+    inner: Arc<Inner>,
+}
+
+#[derive(Clone)]
+pub struct Listener {
+    inner: Arc<Inner>,
+}
+
+struct Inner {
+    complete: AtomicBool,
+    tasks: Mutex<Vec<Waker>>,
+}
+
+impl Unpin for Trigger {}
+impl Unpin for Listener {}
+
+impl Trigger {
+    pub fn trigger(&self) {
+        if self.inner.complete.swap(true, Ordering::SeqCst) {
+            return;
+        }
+        // This code will only be executed once. No matter the amount of `Trigger` clones or calls
+        // to `trigger()`, thanks to the atomic swap above.
+        let mut tasks_guard = self
+            .inner
+            .tasks
+            .lock()
+            .expect("Some Trigger/Listener has panicked");
+        let tasks = mem::replace(&mut *tasks_guard, Vec::new());
+        mem::drop(tasks_guard);
+        for task in tasks {
+            task.wake()
+        }
+    }
+}
+
+impl std::future::Future for Listener {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
+        if self.inner.complete.load(Ordering::SeqCst) {
+            return Poll::Ready(());
+        }
+
+        let mut tasks = self
+            .inner
+            .tasks
+            .lock()
+            .expect("Some Trigger/Listener has panicked");
+
+        // If the trigger completed while we waited for the lock, skip adding our waker to the list
+        // of tasks.
+        if self.inner.complete.load(Ordering::SeqCst) {
+            Poll::Ready(())
+        } else {
+            tasks.push(cx.waker().clone());
+            Poll::Pending
+        }
+    }
+}
