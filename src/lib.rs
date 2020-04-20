d3af316b421882c02d33b8f6588ae31b0be4793f
Linus Färnstrand <faern@faern.net>
2020-04-20 23:29:11 +0200

    Improve documentation and examples

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! Simple triggers that allows triggering a one time event in another task/thread.
+//! Triggers for one time events between tasks and threads.
 //!
 //! The mechanism consists of two types, the [`Trigger`] and the [`Listener`]. They come together
 //! as a pair. Much like the sender/receiver pair of a channel. The trigger half has a
@@ -23,43 +23,82 @@
 //! #[tokio::main]
 //! async fn main() {
 //!     let (trigger, listener) = triggered::trigger();
-//!     tokio::spawn(async {
+//!
+//!     let task = tokio::spawn(async {
+//!         // Blocks until `trigger.trigger()` below
 //!         listener.await;
-//!         println!("Trigger went off!");
+//!
+//!         println!("Triggered async task");
 //!     });
+//!
+//!     // This will make any thread blocked in `Listener::wait()` or async task awaiting the
+//!     // listener continue execution again.
 //!     trigger.trigger();
+//!
+//!     let _ = task.await;
 //! }
 //! ```
 //!
 //! An example showing a trigger/listener pair being used to gracefully shut down some async
-//! server instances on a Ctrl+C event, where only an immutable `Fn` closure is accepted:
+//! server instances on a Ctrl-C event, where only an immutable `Fn` closure is accepted:
 //!
-//! ```ignore
+//! ```
+//! # use std::future::Future;
+//! # type Error = Box<dyn std::error::Error>;
+//! # struct SomeServer;
+//! # impl SomeServer {
+//! #    fn new() -> Self { SomeServer }
+//! #    async fn serve_with_shutdown_signal(self, s: impl Future<Output = ()>) -> Result<(), Error> {Ok(())}
+//! #    async fn serve(self) -> Result<(), Error> {Ok(())}
+//! # }
+//!
 //! #[tokio::main]
-//! async fn main() -> Result<(), Box<dyn std::error::Error> {
-//!     let (shutdown_trigger, shutdown_signal) = triggered::trigger();
+//! async fn main() -> Result<(), Error> {
+//!     let (shutdown_trigger, shutdown_signal1) = triggered::trigger();
 //!
+//!     // A sync `Fn` closure will trigger the trigger when the user hits Ctrl-C
 //!     ctrlc::set_handler(move || {
 //!         shutdown_trigger.trigger();
 //!     }).expect("Error setting Ctrl-C handler");
 //!
-//!     let server1_task = tokio::spawn(async {
-//!         SomeServer::new().serve_with_shutdown_signal(shutdown_signal.clone());
-//!     });
-//!     let server2_task = tokio::spawn(async {
-//!         SomeOServer::new().serve_with_shutdown_signal(shutdown_signal);
+//!     // If the server library has support for something like a shutdown signal:
+//!     let shutdown_signal2 = shutdown_signal1.clone();
+//!     let server1_task = tokio::spawn(async move {
+//!         SomeServer::new().serve_with_shutdown_signal(shutdown_signal1).await;
 //!     });
 //!
-//!     server1_task.await?;
-//!     server2_task.await?;
+//!     // Or just select between the long running future and the signal to abort it
+//!     tokio::select! {
+//!         server_result = SomeServer::new().serve() => {
+//!             eprintln!("Server error: {:?}", server_result);
+//!         }
+//!         _ = shutdown_signal2 => {}
+//!     }
+//!
+//!     let _ = server1_task.await;
+//!     Ok(())
 //! }
 //! ```
 //!
-//! # Comparison to channels
+//! # Rust Compatibility
+//!
+//! Will work with at least the two latest stable Rust releases. This gives users at least six
+//! weeks to upgrade their Rust toolchain after a new stable is released.
+//!
+//! The current MSRV can be seen in `travis.yml`. Any change to the MSRV will be considered a
+//! breaking change and listed in the changelog.
+//!
+//! # Comparison with similar primitives
+//!
+//! ## Channels
+//!
+//! The event triggering primitives in this library is somewhat similar to channels. The main
+//! difference and why I developed this library is that
 //!
 //! The listener is somewhat similar to a `futures::channel::oneshot::Receiver<()>`. But it:
-//!  * Is not fallible - Implements `Future<()>` instead of `Future<(), Canceled>`
-//!  * Implements `Clone` - Any number of listeners can wait for the same oneshot event
+//!  * Is not fallible - Implements `Future<Output = ()>` instead of
+//!    `Future<Output = Result<T, Canceled>>`
+//!  * Implements `Clone` - Any number of listeners can wait for the same event
 //!  * Has a sync [`Listener::wait`] - Both synchronous threads, and asynchronous tasks can wait
 //!    at the same time.
 //!
@@ -68,7 +107,16 @@
 //!  * Is not fallible - The trigger does not care if there are any listeners left
 //!  * Does not consume itself on send, instead takes `&self` - So can be used
 //!    in situations where it is not owned or not mutable. For example in `Drop` implementations
-//!    or callback closures that are limited to `Fn` or `FnMut`
+//!    or callback closures that are limited to `Fn` or `FnMut`.
+//!
+//! ## `futures::future::Abortable`
+//!
+//! One use case of these triggers is to abort futures when some event happens. See examples above.
+//! The differences include:
+//!  * A single handle can abort any number of futures
+//!  * Some futures are not properly cleaned up when just dropped the way `Abortable` does it.
+//!    These libraries sometimes allows creating their futures with a shutdown signal that triggers
+//!    a clean abort. Something like `serve_with_shutdown(signal: impl Future<Output = ()>)`.
 
 #![deny(unsafe_code)]
 #![deny(rust_2018_idioms)]
